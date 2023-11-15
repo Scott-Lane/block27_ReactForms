b814f8653e2f0f2ccd3a174ced10f434601917eb
Scott-Lane <scott@DESKTOP-HOKH2LL>
2023-11-14 20:37:23 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,16 @@
-
 import './App.css'
+import Authenticate from './components/Authenticate'
+import SignUpForm from './components/SignUpForm'
+
+function App() {
+
+  return (
+    <>
+      <SignUpForm />
+      <Authenticate />
+      
+    </>
+  );
+}
+
+export default App
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,52 @@
+import {useState} from 'react'
+
+
+export default function SignUpForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username, password
+                })
+            });
+
+            const info = await response.json();
+            console.log(info);
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+
+    return (
+        <>
+            <h2>Sign Up!</h2>
+            
+            {error && <p>{error}</p>}
+
+            <form onSubmit={handleSubmit}>
+                <label>Username: <input value={username} onChange={(event) => {
+                    // console.log(event.target.value);
+                    setUsername(event.target.value);
+                }} 
+                    id="user-name" /></label> <br />
+                
+                <label>Password: <input value={password} onChange={(event) => {
+                    // console.log(event.target.value);
+                    setPassword(event.target.value);
+                }}
+                    type="password" id="user-password" /></label> <br />
+                
+                <button type="submit">Submit</button>
+        </form>
+         </>
+    );
+}
